0bfa7e0e647c8098611e32ddb513f465920fe399
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-07-26 14:19:42 -0600

    (SERVER.JS) added line to address error message; (DB) added array brackets to contain content

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const uniqid = require("uniqid");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
